28bebaf645f21b62720517a34dcbab293bed23be
Jacco Gritter <jacco.gritter@gmail.com>
2019-06-11 16:31:13 +0200

    show books much improved plus edit/delete buttons

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -5,13 +5,41 @@
 
 <h2>Welkom in de boekenkast</h2>
 
-<ul>
-@foreach ($books as $book)
+<table class="table">
 
-    <li>{{ $book->titel }}</li>
+    <tablehead>
+        <tr>
+            <th>Auteur</th>
+            <th>Titel</th>
+            <th>Taal</th>
+            <th>Pagina's</th>
+            <th></th><th></th>
+        </tr>
+    </tablehead>
 
-@endforeach
-</ul>
+    <tablebody>
+
+    @foreach ($books as $book)
+        <tr>
+            <td>{{ $book->auteur }}</td>
+            <td>{{ $book->titel }}</td>
+            <td>{{ $book->taal }}</td>
+            <td>{{ $book->aantal_paginas }}</td>
+            <td>
+                <a href="{{ route('books.edit', $book->id) }}" class="btn btn-primary">Edit</a>
+            </td>
+            <td>
+                <form action="{{ route('books.destroy', $book->id)}}" method="post">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit">Wis</button>
+                </form>
+            </td>
+        </tr>
+    @endforeach
+
+    </tablebody>
+</table>
 
 <a href="/books/create">Voeg boek toe</a>
 
