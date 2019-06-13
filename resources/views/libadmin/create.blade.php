b304df135157dc9cbc835470fd9bbaeddbaa478d
Jacco Gritter <jacco.gritter@gmail.com>
2019-06-13 13:33:31 +0200

    old fields show after submitting form with errors

--- resources/views/libadmin/create.blade.php
+++ resources/views/libadmin/create.blade.php
@@ -13,17 +13,17 @@
 
         <div class="form-group">
             <label for="titel">Titel</label>
-            <input type="text" class="form-control" name="titel" value="" required>
+            <input type="text" class="form-control" name="titel" value="{{old('titel')}}" required>
         </div>
 
         <div class="form-group">
             <label for="auteur">Auteur</label>
-            <input type="text" class="form-control" name="auteur" value="" required >
+            <input type="text" class="form-control" name="auteur" value="{{old('auteur')}}" required >
         </div>
 
         <div class="form-group">
         <label for="taal">Taal</label>
-            <select class="form-control" id="taal" name="taal">
+            <select class="form-control" id="taal" name="taal" {{old('taal')}}>
                 <option value="nl">Nederlands</option>
                 <option value="en">Engels</option>
                 <option value="fr">Frans</option>
@@ -33,20 +33,28 @@
 
         <div class="form-group">
             <label for="aantal_paginas">Aantal paginas</label>
-            <input type="number" class="form-control" name="aantal_paginas" value="" required>
+            <input type="number" class="form-control" name="aantal_paginas" value="{{old('aantal_paginas')}}" required>
         </div>
 
         <div class="form-group">
             <label for="isbn">ISBN nummer</label>
-            <input type="text" class="form-control" name="isbn" value="" required >
+            <input type="number" class="form-control" name="isbn" value="{{old('isbn')}}" required >
         </div>
 
         <div class="form-group">
             <label for="uitgeleend_aan">Uitgeleend aan</label>
-            <input type="text" class="form-control" name="uitgeleend_aan" value="" placeholder = "optioneel" >
+            <input type="text" class="form-control" name="uitgeleend_aan" value="{{old('uitgeleend_aan')}}" placeholder = "optioneel" >
         </div>
 
         <button type="submit" class="btn btn-primary mb-1">Voeg toe</button>
+
+        <div class="bg-danger text-light">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li> {{ $error }} </li>
+                @endforeach
+            </ul>
+        </div>
 
         </form>
   
