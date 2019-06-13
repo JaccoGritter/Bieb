0fecc6956319593734d4cd20305063b8cf9cef69
Jacco Gritter <jacco.gritter@gmail.com>
2019-06-13 13:30:08 +0200

    fillable fields added to Book model

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = [
+        'titel',
+        'auteur',
+        'taal',
+        'aantal_paginas',
+        'isbn',
+        'uitgeleend_aan'       
+    ];
 }
+
