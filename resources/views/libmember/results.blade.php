40104e514ad471b30b60dfd932115a2068c3a65a
Jacco Gritter <jacco.gritter@gmail.com>
2019-06-16 14:34:00 +0200

    book search for members added

--- resources/views/libmember/results.blade.php
+++ resources/views/libmember/results.blade.php
@@ -1,1 +1,34 @@
-Results here!
+@extends('layouts\libmemberLayout')
+
+@section('content')
+
+<table class="table table-hover">
+
+    <tablehead>
+        <tr>
+            <th>Auteur</th>
+            <th>Titel</th>
+            <th>Taal</th>
+            <th>Genre</th>
+            <th>Pagina's</th>
+            <th>ISBN</th>
+        </tr>
+    </tablehead>
+
+    <tablebody>
+
+    @foreach ($books as $book)
+    <tr>
+        <td>{{ $book->auteur }}</td>
+        <td>{{ $book->titel }}</a></td>
+        <td>{{ $book->taal }}</td>
+        <td>{{ $book->genre }}</td>
+        <td>{{ $book->aantal_paginas }}</td>
+        <td>{{ $book->isbn }}</td>
+    </tr>
+    @endforeach
+
+    </tablebody>
+</table>
+
+@endsection
