a7432922ca9fe83e647a0ff6b36e85ee2655aaa4
Jacco Gritter <jacco.gritter@gmail.com>
2019-06-18 10:50:49 +0200

    member can log out

--- resources/views/libmember/memberindex.blade.php
+++ resources/views/libmember/memberindex.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
 <p>{{ $member->voornaam}}&nbsp;{{ $member->achternaam }} is ingelogd</p>
+<a href="{{ route ('members.memberLogin') }}">Log uit</a>
 
 <h1>Welkom {{ $member->voornaam }}</h1>
 
@@ -12,4 +13,6 @@
 <form action="{{ route ('books.findBooks') }}" method="get">
 Auteur&nbsp<input type="text" name="auteur">&nbsp<button type="submit" class="btn btn-primary">zoek</button>
 </form>
+
+<a href="{{ route ('libmember.lentbooks') }}">Bekijk boeken in mijn bezit</a>
 @endsection
