2cee3ef6ae877db5e4d19c508e2cf94fe153871c
Jacco Gritter <jacco.gritter@gmail.com>
2019-06-20 09:39:41 +0200

    added if/else to check if any books are lent

--- resources/views/libmember/lentbooks.blade.php
+++ resources/views/libmember/lentbooks.blade.php
@@ -3,14 +3,21 @@
 @section('content')
  
 
+@if ($lentBooks->count())
 
-<h3>Boeken die je momenteel geleend hebt:</h3>
+    <h3>Boeken die je momenteel geleend hebt:</h3>
 
-@foreach($lentBooks as $lentBook)
+    @foreach($lentBooks as $lentBook)
 
-    <p>{{ $lentBook->titel }} van {{ $lentBook->auteur }} geleend sinds {{ $lentBook->pivot->lent_from }} </p>
- 
-@endforeach
+        <p>{{ $lentBook->titel }} van {{ $lentBook->auteur }} geleend sinds {{ $lentBook->pivot->lent_from }} </p>
+    
+    @endforeach
+
+@else
+
+    <h3>Je hebt geen boeken geleend</h3>    
+
+@endif
 
 
 <div style="margin: 19px;">
