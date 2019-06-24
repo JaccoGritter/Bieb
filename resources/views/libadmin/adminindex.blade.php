05256437f4e5073ed9402e44bb505c4bef0f65f4
Jacco Gritter <jacco.gritter@gmail.com>
2019-06-24 09:04:08 +0200

    small cosmetic change

--- resources/views/libadmin/adminindex.blade.php
+++ resources/views/libadmin/adminindex.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts\libadminLayout')
 
 @section('content')
+<br>
+<h4>Maak een keuze:</h4>
+<br>
+<h5><a href="{{ route('books.viewallbooks') }}">Naar alle boeken</a></h5>
 
-<a href="{{ route('books.viewallbooks') }}">Naar alle boeken</a><br>
-<a href="{{ route('users.index') }}">Naar alle leden</a>
+<h5><a href="{{ route('users.index') }}">Naar alle leden</a></h5>
 
 @endsection('content')
