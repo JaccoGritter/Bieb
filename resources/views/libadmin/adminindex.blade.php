014d8d6780a93fb0e596e5de2e171196c47e0bd0
Jacco Gritter <jacco.gritter@gmail.com>
2019-06-26 09:29:52 +0200

    .

--- resources/views/libadmin/adminindex.blade.php
+++ resources/views/libadmin/adminindex.blade.php
@@ -4,8 +4,5 @@
 <br>
 <h4>Maak een keuze:</h4>
 <br>
-<h5><a href="{{ route('books.viewallbooks') }}">Naar alle boeken</a></h5>
-
-<h5><a href="{{ route('users.index') }}">Naar alle leden</a></h5>
 
 @endsection('content')
